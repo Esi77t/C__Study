34579b88dd94b561f5b112183c613174e028cfc5
Esi77t <2089651@naver.com>
2025-09-08 22:04:46 +0900

    push

---
+++ csharp/day07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 백준 10926번
+        string str = Console.ReadLine();
+
+        Console.WriteLine($"{str}" + "??!");
+
+
+        // 백준 18108번
+
+        int year = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(year - 543);
+    }
+}
